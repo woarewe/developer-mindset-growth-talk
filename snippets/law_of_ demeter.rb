f9d6315cb00593d92a98070e9d71b7ba46ac0747
Rostislav Zhuravsky <rostislav.zhuravsky@gmail.com>
2021-11-26 04:01:35 +0300

    Add law of demeter

---
+++ snippets/law_of_ demeter.rb
@@ -0,0 +1,32 @@
+# Bad
+class Address
+  attr_reader :city
+end
+
+class Bulding
+  attr_reader :address
+end
+
+def building_city(building)
+  building && building.address && building.address.city
+end
+
+# Good
+
+class Address
+  attr_reader :city
+end
+
+class Bulding
+  attr_reader :address
+
+  def address_city
+    address && address.city
+  end
+end
+
+def building_city(building)
+  building && building.address_city
+end
+
+
