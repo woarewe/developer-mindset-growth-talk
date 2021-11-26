17bbe36555f3cb6942bca6598249b54b8a8423a5
Rostislav Zhuravsky <rostislav.zhuravsky@gmail.com>
2021-11-26 04:59:41 +0300

    Fix

--- snippets/positional_args_vs_keyword_args.rb
+++ snippets/positional_args_vs_keyword_args.rb
@@ -1,26 +1,28 @@
 # Bad
 
 class Balance
-  attr_reader :amount, :currency
+  attr_reader :amount, :currency, :owner
 
-  def initialize(amount, currency)
+  def initialize(amount, currency, owner)
     @amount = amount
     @currency = currency
+    @owner = owner
   end
 end
 
-Balance.new(1000, 'USD')
+Balance.new(1000, 'USD', 'John')
 
 # Good
 
 class Balance
-  attr_reader :amount, :currency
+  attr_reader :amount, :currency, :owner
 
-  def initialize(amount:, currency:)
+  def initialize(amount:, currency:, owner:)
     @amount = amount
     @currency = currency
+    @owner = owner
   end
 end
 
-Balance.new(amount: 1000, currency: 'USD')
-Balance.new(currency: 'USD', amount: 1000)
+Balance.new(amount: 1000, currency: 'USD', owner: 'John')
+Balance.new(currency: 'USD', owner: 'John', amount: 1000)
