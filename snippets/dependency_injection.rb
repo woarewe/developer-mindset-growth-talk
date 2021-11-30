9446931921d2b40cb793f2c62272d615c34e54d7
woarewe <rostislav.zhuravsky@gmail.com>
2021-12-01 02:14:46 +0300

    Improve DI example

--- snippets/dependency_injection.rb
+++ snippets/dependency_injection.rb
@@ -1,7 +1,7 @@
 # Bad
 
 class SyncService
-  def perform(params)
+  def call(params)
     # does some logic
     HTTP::Client.new.post('/data', params.fetch(:data))
   end
@@ -12,11 +12,11 @@ end
 class SyncService
   attr_reader :http_client
 
-  def initiailze(http_client:  HTTP::Client.new)
+  def initiailze(http_client: HTTP::Client.new)
     @http_client = http_client
   end
 
-  def perform(params)
+  def call(params)
     # does some logic
     http_client.post('/data', params.fetch(:data))
   end
